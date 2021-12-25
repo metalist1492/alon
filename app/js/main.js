8152b274e2c9da121ebe380c0326728d7cfdf7e6
metalist1492 <metalist1492@gmail.com>
2021-12-25 19:01:39 +0200

    added main page and about page

---
+++ app/js/main.js
@@ -0,0 +1,26 @@
+$(function () {
+
+
+  const mySwiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 120,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  const swiperPrev = document.getElementById('swiperPrev');
+  console.log(swiperPrev);
+  const swiperNext = document.getElementById('swiperNext');
+  console.log(swiperNext);
+  swiperPrev.addEventListener('click', () => {
+    mySwiper.slidePrev();
+  });
+  swiperNext.addEventListener('click', () => {
+    mySwiper.slideNext();
+  });
+
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -30,6 +30,7 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/swiper/swiper-bundle.min.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
