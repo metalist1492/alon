390bd7b161d7c5a9c45c9a9c8079b8b828ebf473
metalist1492 <metalist1492@gmail.com>
2021-12-29 13:18:09 +0200

    finish адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -7,11 +7,25 @@ $(function () {
 
   const mySwiper = new Swiper('.swiper', {
     loop: true,
-    slidesPerView: 3,
-    spaceBetween: 120,
+    slidesPerView: 1,
+    spaceBetween: 20,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 120,
+      },
     },
   });
 
